de35dd11164a4be3a8567683c1490eb6810fe3ae
Travis Fletcher <travis@tfletch.tech>
2021-10-24 11:53:27 -0400

    switched to parcel for tree shaking

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { html } from 'htm/preact'
+import { render } from 'preact'
+
+const App = props => html`
+  <p> Under Construction <//>
+`
+
+render(html`<${App} />`, document.body)
--- util/react-htm.js
+++
@@ -1,4 +0,0 @@
-import htm from '_htm'
-import React from 'react'
-
-export const html = htm.bind(React.createElement)
