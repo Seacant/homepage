9f516a69a879d6130ac306e04f89320d406bc34b
Seacant <travis@tfletch.tech>
2022-10-31 20:54:54 +0000

    Add first blog post

---
+++ pages/blog/getting-started.tsx
@@ -0,0 +1,231 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import styles from './blog.module.css'
+import profilePic from '../../public/me.png'
+import { stripIndent, html } from 'common-tags'
+
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+
+const Article = () => <>
+  <article className={styles.article}>
+    <h1> Learning to &lt;code/&gt; </h1>
+    <div className={styles.article_info}>
+      <Image className={styles.avatar} src={profilePic} height="32" width="32" alt="Picture of the author"></Image>
+      <address> By Travis Fletcher </address>
+      on 
+      <time dateTime="2022-10-30"> 30 Oct, 2022 </time>
+    </div>
+
+    <p>
+      This is a coding blog, so it makes sense to start out by making sure we
+      can represent code. My <code>site:developer.mozilla.org</code>-enhanced
+      googling skills tell me that <code>{ html`<pre /> + <code />` }</code> is
+      all the rage, so let's give that a try.
+    </p>
+
+    <h2> The Basics </h2>
+    <pre>
+      <code>{ stripIndent`
+        if ( window.navigator ) {
+          console.log("Hello, user!")
+        } else {
+          console.error("There's nobody to read me :(")
+        }
+      `}</code>
+    </pre>
+
+    <p>
+      That looks good for vanilla Javascript, but what about JSX? The block
+      above actually looks like
+    </p>
+
+    <pre>
+      <code>{ html`
+        <pre>
+          <code>{ stripIndent\`
+            if ( window.navigator ) {
+              console.log("Hello, user!")
+            } else {
+              console.error("There's nobody to read me :(")
+            }
+          \`}</code>
+        </pre>
+      `}</code>
+    </pre>
+
+    <p>
+      <code>stripIndent`...`</code> is a <a 
+        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals"
+      >Tagged Template Literal</a> and allows us to write our code at the
+      current indent level without the spacing making it into the final doc,
+      while still maintaining its internal indentation. Super neat!
+    </p>
+
+    <p>
+      In a similar vein, <em>that</em> block actually looks like
+    </p>
+
+
+    <pre>
+      <code>{ html`
+        <pre>
+          <code>{ html\`
+            ...
+          \`}</code>
+        </pre>
+      `}</code>
+    </pre>
+
+    <p>
+      Where <code>html`...`</code> is a similar tagged template which helps us
+      automatically encode our alligators into their html-escaped equivalent.
+      Looking alright so far...
+    </p>
+
+    <h2> Syntax Highlighting </h2>
+    <p>
+      I'm a strong believer in good web fundamentals, so any feature
+      enhancement we add must have reasonable fallback behavior for people
+      who choose to turn off Javascript or even who use browsers (such as
+      links) who don't even support the notion. But I also can't stand reading
+      un-highlighted code, and I'm sure you all would appreciate some color in
+      your life as well. Let's see what we can do about that.
+    </p>
+
+    <p>
+      The first result for "react syntax highlighter" is,
+      unsurprisingly, <code>react-syntax-highlighter</code>, which seems to
+      support progressively highlighting if the client has JS enabled.
+    </p>
+
+    <SyntaxHighlighter language="javascript" useInlineStyles={false}>{ stripIndent`
+        if ( window.navigator ) {
+          console.log("Hello, user!")
+        } else {
+          console.error("There's nobody to read me :(")
+        }
+    `}</SyntaxHighlighter>
+
+    <p>
+      Not bad! In JSX, the above looks like
+    </p>
+
+    <SyntaxHighlighter language="javascript" useInlineStyles={false}>{ stripIndent`
+      /* Contains our 2 styles behind a @media prefers-color-scheme selector */
+      import '../highlight.css'
+
+      // Then, anywhere
+      import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+      <SyntaxHighlighter language="javascript" useInlineStyles={false}>{ stripIndent\`
+          if ( window.navigator ) {
+            console.log("Hello, user!")
+          } else {
+            console.error("There's nobody to read me :(")
+          }
+      \`}</SyntaxHighlighter>
+    `}</SyntaxHighlighter>
+
+    <p>
+      Both of which render from the server as completely valid {
+        html`<pre><code>`
+      } blocks with JS only being required to tag and color them.
+    </p>
+
+    <h2> Thinking outside the box </h2>
+
+    <p>
+       That scrollbar doesn't looks very good, considering how much free
+       real estate we're wasting in our gutters. Let's see if we can't get
+       something akin to 'full-bleed' working.
+    </p>
+
+    <SyntaxHighlighter className={styles.full_bleed} language="javascript" useInlineStyles={false}>{ stripIndent`
+      import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+      <SyntaxHighlighter language="javascript" useInlineStyles={false}>{ stripIndent\`
+          if ( window.navigator ) {
+            console.log("Hello, user!")
+          } else {
+            console.error("There's nobody to read me :(")
+          }
+      \`}</SyntaxHighlighter>
+    `}</SyntaxHighlighter>
+
+    <p>
+      And, just to test, if our content is <em>really</em> long...
+    </p>
+
+    <SyntaxHighlighter className={styles.full_bleed} language="javascript" useInlineStyles={false}>{ stripIndent`
+    if(you_use_really_long_variable_names_and_hate_newlines_because_youre_a_l33t_hacker_man){console.log("what a cool guy")}
+    `}</SyntaxHighlighter>
+
+
+    <p>
+      This all works because of the amazing CSS Grid, which is nice enough to
+      let us break out of the column we're constrained to as will. In its
+      entirety, the code that allows all of this to work is as follows
+    </p>
+
+    <SyntaxHighlighter language="css" useInlineStyles={false}>{ stripIndent`
+      // styles.css
+      main {
+        display: grid;
+        grid-template-columns:
+          1fr
+          min(65ch, 100%)
+          1fr;
+      }
+
+      main > * {
+        grid-column: 2;
+      }
+
+      // blog.module.css
+      .article {
+        text-align: justify;
+        display: contents;
+      }
+      .article > * {
+        grid-column: 2;
+      }
+
+      .full_bleed {
+        width: 100%;
+        grid-column: 1 / -1;
+
+        display: flex;
+        justify-content: center;
+      }
+    `}</SyntaxHighlighter>
+
+    <p>
+      This is almost entirely inspired by <a 
+        href="https://css-tricks.com/full-bleed/"
+      >CSS Tricks's Full Bleed article</a> with the tricky bit being that we
+      want to declare our layout at the <code>main</code> tag but our semantic
+      HTML-ness means we are surrounded by an <code>article</code> before we get
+      to the actual content. By declaring the article as <code>display: contents</code>,
+      we are able to hide the article tag from the DOM and promote its children
+      to be in the grid instead.
+    </p>
+  </article>
+</>
+
+export const GettingStartedCard = () => 
+  <article className={styles.blog_card}>
+    <h2> 
+      <Link href="blog/getting-started">
+        Learning to &lt;code/&gt;
+      </Link>
+    </h2>
+    <p>
+      Is manually coded HTML the new CMS Hotness? Probably not, but it's pretty
+      fun to play around with. Let's get our toes wet by taking a look at the
+      cool CSS we can use to spice up our blog posts.
+    </p>
+  </article>
+
+export default Article
