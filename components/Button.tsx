9db18cba3aadbe82291bd3b4943cb3cc9eef979a
Travis Fletcher <travis@tfletch.tech>
2023-09-05 03:18:07 +0000

    converted resume to styled component
    and minor format-content changes

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const Button = styled.button`
   padding: 1ch;
   border-radius: 4px;
+  border: none;
+  box-shadow: 0 0 0 1px var(--main-prose-color);
+  color: inherit;
 `;
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "./resume.module.css";
 import { Button } from "../components/Button";
 import styled from "styled-components";
 
@@ -8,25 +7,96 @@ const PdfButton = styled(Button)`
   margin-bottom: 1ch;
 
   @media print {
-    display: none'
+    display: none;
   }
 `;
+
+const RetroLetter = styled.article`
+  @media print {
+    margin-left: -.25in;
+    font-size: 14px;
+    line-height: 14px;
+    color: black;
+
+    & a {
+      text-decoration: none;
+      color: black;
+    }
+  }
+
+
+  /* since we are taking away the normal site gutters, add back 
+   * something reminiscent of real paper margins
+  */
+  @media (max-width: 8.5in) {
+    margin-left: 0.5in;
+    margin-right: 0.5in;
+  }
+
+  max-width: 7.5in;
+  display: flex;
+  flex-direction: column;
+  grid-column: -1 / 1;
+  gap: 1ch;
+  margin: auto;
+
+  font-family: var(--mono-font);
+  line-height: 1.2em;
+  font-weight: 500;
+
+  & ul {
+    margin: .25em;
+  }
+`;
+
+const GenericSection = styled.section`
+  display: contents;
+  break-before: auto;
+  break-inside: avoid;
+`;
+
+const SectionProse = styled.div`
+  margin-left: 2ch;
+`
+
+const Introduction = styled(GenericSection)`
+  display: flex;
+  justify-content: space-between;
+`
+
+const ContactInfo = styled.address`
+  text-align: right;
+`;
+
+const Year = styled.span<{single?: true}>`
+  float: right;
+  
+  /* 20xx-20xx is 9 characters */
+  ${({single}) => single ? 'width: 5ch' : 'width: 9ch'}
+`
+
+const Header = styled.h2`
+  text-transform: uppercase;
+  margin: 0;
+  line-height: 1.1em;
+`;
+
 
 const Resume = () => (
   <>
     <Head>
       <title>Travis' Resume</title>
     </Head>
-    <PdfButton className={styles.banner} onClick={() => window.print()}>
+    <PdfButton onClick={() => window.print()}>
       Psst! Need a PDF?
     </PdfButton>
-    <article className={[styles.letter, styles.retro].join(" ")}>
-      <section className={styles.block_left}>
+    <RetroLetter>
+      <Introduction>
         <div>
-          <h2> Travis Fletcher </h2>
+          <Header> Travis Fletcher </Header>
           <span> Full Stack Senior Software Engineer </span>
         </div>
-        <address className={styles.block_right}>
+        <ContactInfo>
           <div>
             <a href="github.com/Seacant"> GitHub @ Seacant </a>
           </div>
@@ -36,60 +106,71 @@ const Resume = () => (
           <div>
             <a href="mailto:contact@tfletch.tech"> contact@tfletch.tech </a>
           </div>
-        </address>
-      </section>
-      <section>
-        <h2>Technical Skills</h2>
+        </ContactInfo>
+      </Introduction>
+      <GenericSection>
+        <Header>Technical Skills</Header>
         <div>
           Languages:
-          <div> Typescript, Ruby, HTML/CSS, Perl, SQL, Rust </div>
+          <SectionProse> Typescript, Ruby, HTML/CSS, Perl, SQL, Rust </SectionProse>
         </div>
         <div>
           Technologies:
-          <div> React, Ruby on Rails, Terraform, AWS </div>
+          <SectionProse> React, Ruby on Rails, Terraform, AWS </SectionProse>
         </div>
         <div>
           Techniques:
-          <div>
-            {" "}
+          <SectionProse>
             Domain-Driven Design, Functional Programming, Test-Driven
-            Development{" "}
-          </div>
+            Development
+          </SectionProse>
         </div>
         <div>
           Specialties:
-          <div>
-            {" "}
+          <SectionProse>
             Software/Feature Architecture, Performance & Scalability,
-            Accessibility, Machine Learning{" "}
-          </div>
+            Accessibility, Machine Learning
+          </SectionProse>
         </div>
-      </section>
-      <section>
-        <h2>Education</h2>
+      </GenericSection>
+      <GenericSection>
+        <Header>Education</Header>
         <div>
           Loveland High School
-          <span className={styles.right}>2014-2017</span>
+          <Year>2014-2017</Year>
         </div>
 
         <div>
           Northern Kentucky University
-          <span className={styles.right}>2017-2021</span>
-          <div>Data Science Major & Management Minor </div>
+          <Year>2017-2021</Year>
+          <SectionProse>Data Science Major & Management Minor </SectionProse>
         </div>
-      </section>
-      <section>
-        <h2>Employment</h2>
+      </GenericSection>
+      <GenericSection>
+        <Header>Employment</Header>
+        <div>
+          ExamSpark
+          <SectionProse>
+            Senior Software Engineer
+            <Year>2023-</Year>
+          </SectionProse>
+          <ul>
+            <li>
+              Developed in cutting-edge cloud-native AWS Amplify environment via
+              full-stack Typescript
+            </li>
+          </ul>
+        </div>
         <div>
           Instructure
-          <div>
+          <SectionProse>
             Senior Software Engineer
-            <span className={styles.right}>2022-&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          </div>
-          <div>
+            <Year>2022-2023</Year>
+          </SectionProse>
+          <SectionProse>
             Software Engineer
-            <span className={styles.right}>2021-2022</span>
-          </div>
+            <Year>2021-2022</Year>
+          </SectionProse>
           <ul>
             <li>
               Maintained and scaled Ruby on Rails app with 10000+ Rps across
@@ -100,88 +181,80 @@ const Resume = () => (
               customer needs
             </li>
             <li>
-              {" "}
-              Led technical discussions during Software Architecture Planning{" "}
+              Led technical discussions during Software Architecture Planning
             </li>
             <li>
-              {" "}
-              Championed accessible web design during front-end discussions{" "}
+              Championed accessible web design during front-end discussions
             </li>
           </ul>
         </div>
         <div>
           Certica Solutions
-          <div>
+          <SectionProse>
             Software Engineer
-            <span className={styles.right}> 2019-2021 </span>
-          </div>
-          <div>
+            <Year>2019-2021</Year>
+          </SectionProse>
+          <SectionProse>
             Junior Software Engineer
-            <span className={styles.right}> 2015-2019 </span>
-          </div>
+            <Year>2015-2019</Year>
+          </SectionProse>
           <ul>
             <li>
-              {" "}
               Provide front-end website and database support to both internal
-              and external clients{" "}
+              and external clients
             </li>
-            <li> Develop unit tests for legacy monolithic codebase </li>
+            <li>Develop unit tests for legacy monolithic codebase</li>
             <li>
-              {" "}
-              Maintain a RESTful JSON API with a full regression testing suite{" "}
+              Maintain a RESTful JSON API with a full regression testing suite
             </li>
             <li> Use domain-driven design to retrofit OOP to codebase </li>
           </ul>
         </div>
         <div>
           Northern Kentucky University
-          <div>
+          <SectionProse>
             Research Assistant
-            <span className={styles.right}> 2018-2019 </span>
-          </div>
+            <Year> 2018-2019 </Year>
+          </SectionProse>
           <ul>
             <li>
-              {" "}
               Developed an Internet of Things security management solution, Java
-              Experience{" "}
+              Experience
             </li>
             <li> Presented findings at NKU Celebration of Student Research </li>
           </ul>
         </div>
-      </section>
-      <section>
-        <h2> Experience </h2>
+      </GenericSection>
+      <GenericSection>
+        <Header> Experience </Header>
         <div>
           FTC Robotics
-          <span className={styles.right}> 2015-2017 </span>
-          <div> Lead Programmer </div>
-          <div> Java, Android Studio, Github, Gradle, and Vim </div>
-          <div>
-            {" "}
-            Created a robot using Java to compete in predetermined challenges{" "}
-          </div>
+          <Year>2015-2017</Year>
+          <SectionProse> Lead Programmer </SectionProse>
+          <SectionProse> Java, Android Studio, Github, Gradle, and Vim </SectionProse>
+          <SectionProse>
+            Created a robot using Java to compete in predetermined challenges
+          </SectionProse>
         </div>
         <div>
           UC IT Expo
-          <span className={styles.right}> 2016 </span>
-          <div> Lead Programmer; 1st place (High School category) </div>
-          <div>
-            {" "}
-            Developed a low-cost camera mount to follow a lecturer during class{" "}
-          </div>
+          <Year single> 2016 </Year>
+          <SectionProse> Lead Programmer; 1st place (High School category) </SectionProse>
+          <SectionProse>
+            Developed a low-cost camera mount to follow a lecturer during class
+          </SectionProse>
         </div>
         <div>
           RevolutionUC
-          <span className={styles.right}> 2017 </span>
-          <div> 24-hour hackathon </div>
-          <div>
-            {" "}
+          <Year single> 2017 </Year>
+          <SectionProse> 24-hour hackathon </SectionProse>
+          <SectionProse>
             V-Arrrgh - Virtual Reality therapy program for Amblyopia, C#; 1st
-            place{" "}
-          </div>
+            place
+          </SectionProse>
         </div>
-      </section>
-    </article>
+      </GenericSection>
+    </RetroLetter>
   </>
 );
 
