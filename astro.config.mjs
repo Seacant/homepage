f9abf81279fe8c2923a0692b8faf0efbc325b76c
Travis Fletcher <travis@tfletch.tech>
2025-05-23 14:56:21 +0000

    impr: tighten up fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tfletch.tech",
-  devToolbar: {
-    enabled: false,
-  },
-  scopedStyleStrategy: "where",
+    site: "https://tfletch.tech",
+    devToolbar: {
+        enabled: false,
+    },
+    scopedStyleStrategy: "where",
+    server: {
+        allowedHosts: ["sudowoodo"],
+    },
 });
